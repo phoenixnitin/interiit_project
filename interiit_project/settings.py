03621d103e95bcb0ce992358dbeb79097b403b1d
Nitin Kumar <nitinkumar6912@gmail.com>
2017-08-26 21:27:04 -0700

    update : setting.py and requirements merge : master merged with aquatics

--- interiit_project/settings.py
+++ interiit_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v%84l2nn#0ro66at@-ly$nacpz_(qly5=i&2sw^5bp^+_=@&&q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '132.148.91.220', 'interiit.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '132.148.91.220', 'interiit.com', 'server.interiit.com']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 PHONENUMBER_DEFAULT_FORMAT = 'NATIONAL'
